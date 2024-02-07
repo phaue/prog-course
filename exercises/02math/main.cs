7545054d9a89d86f2bff4163ea52539c3612b3da
phaue <philhaue@gmail.com>
2024-02-07 11:59:45 +0100

    .

--- exercises/02math/main.cs
+++ exercises/02math/main.cs
@@ -6,12 +6,26 @@ static class main{
 	static double pot2 = Pow(2.0, 0.2);
 	static double etopi = Pow(E, PI);
 	static double pitoe = Pow(PI, E);
+	
+	
 
 	static int Main(){
+		Write("Task 1:\n");
 		Write($"Sqrt(2)={sqrt2}, and Sqrt(2)^2={sqrt2*sqrt2} should equal 2\n");
 		Write($"2 to the power of 1/5={pot2}, and 2^(1/5)*2^5={Pow(pot2, 5)} should equal 2\n");
-		Write($"e to the power of pi ={etopi}, and e^pi * e^1/pi ={etopi*Pow(E, 1/PI)} should equal e\n");
-		Write($"pi to the power of e ={pitoe}, and pi^e * pi^1/e ={pitoe*Pow(PI, 1/E)} should equal pi\n");
+		Write($"e to the power of pi ={etopi}, and e^pi should equal 23.14 \n");
+		
+		Write($"pi to the power of e ={pitoe}, and pi^e should equal 22.46 \n");
+		
+		Write("\n");
+		Write("Task 2:\n");
+		for(int i=1; i<=10; i++)Write($"gamma({i})={sfuns.fgamma(i)}\n");
+		
+		Write("\n Task 3:\n");
+		for(int i=1; i<=10; i++)Write($"lngamma({i})={sfuns.lngamma(i)}\n");
+		
+		
+
 		return 0;
 	}
 }
