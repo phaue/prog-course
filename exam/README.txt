80cc1723485ecb92c29d339611ad51aa7bbfc22a
phaue <philhaue@gmail.com>
2024-06-24 18:21:14 +0200

    improved testing

--- exam/README.txt
+++ exam/README.txt
@@ -3,7 +3,8 @@ I have also implemented a function that generates a real symmetric positive-defi
 I have implemented the linear equation solver, calculation of determinant and calculation of the inverse matrix.
 
 I have tested these implementations in main_implementations.cs where the result of this testing can be seen in Out_test.txt.
-I have made it possible to alter the size of the matrix subject to the tests through the Makefile.
+The tests have been extended to 50 random matrices to check whether the initial check was a one time result.
+I have also made it possible to alter the size of the matrix subject to the tests through the Makefile.
 
 Then i have tried to see if either of Cholesky-Banaschiewicz or Cholesky-Crout method was to be preferred by timing calculations of large randomly generated matrices.
 For the sizes tested there is no clear advantage of either, they are equally good on random matrices.
--- exam/main_implementation.cs
+++ exam/main_implementation.cs
@@ -54,6 +54,46 @@ class main{
         WriteLine($"is A*inv = 1? {AB.approx(id)}");
 
 
+        WriteLine("\n \n");
+        WriteLine($"Test of 50 such matrices of size {n}x{n} on the 4 different calculations above:\n");
+        int N = 50;
+        int fails = 0;
+        int succes = 0;
+        for(int i=1;i<=N;i++){
+            matrix M = cholesky.generate_posdef_matrix(n);
+            matrix Lbanach = cholesky.banach_decomp(M);
+
+            if(M.approx(Lbanach*Lbanach.T)==false){
+                fails += 1;
+                Error.WriteLine($"Error in cholesky-banach decomposition in iteration{i}");
+            }
+            else succes +=1;
+
+            matrix Lcrout = cholesky.crout_decomp(M);
+            if(M.approx(Lcrout*Lcrout.T)==false){
+                fails += 1;
+                Error.WriteLine($"Error in cholesky-crout decomposition in iteration{i}");
+            }
+            else succes +=1;
+
+            vector B = cholesky.generatevector(n);
+            vector x = cholesky.lineqsolver(M, B);
+            vector solx = M*x;
+		    if(solx.approx(B)==false){
+                fails += 1;
+                Error.WriteLine($"Error in linear equation solver in iteration{i}");
+            }else succes +=1;
+
+            matrix inver = cholesky.inverse(M);
+            matrix one = M*inver;
+            if(one.approx(id)==false){
+                fails += 1;
+                Error.WriteLine($"Error in inverse calculation in iteration{i}");
+            }else succes+=1;
+        }
+        WriteLine("Errors can be found in testerror.txt if any.");
+        WriteLine($"These 200 tests revealed {succes} successful checks & {fails} failed checks");
+
         return 0;
     }//Main
 }//main
